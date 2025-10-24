065219b43cd5a5eb0093acbd593f28da97d3d829
Zeynep Sıla Durak <durakzeynepsila@gmail.com>
2025-10-24 16:19:14 +0300

    Artık fonksiyonun argüman kontrolleri tamamlandı. Push_swap başlatacağım.

--- main.c
+++ main.c
@@ -18,20 +18,38 @@ int main(int ac, char **av)
 	{
 		if (!arg_check(av, ac))
 			print_error();
-		
+		else
+			start_push_swap(av, ac);
+	}
+	else if (ac == 2)
+	{
+		char	**slice_arg;
+		int		i;
 
-			
-		double stack_a[ac - 1];
-		int i = 0;
-		int len;
-		len = ac - 1;
-		while (i < len)
-		{
-			stack_a[i] = *av[ac - 1];
-			ac--;
-			i++;
-		}
+		i = 0;
+		slice_arg = ft_split(av[1], ' ');
+		if (!arg_check(slice_arg, ft_strlen(*slice_arg)))
+			print_error();
+		else
+			start_push_swap(av, ac);
 	}
 	else
 		return (0);
+}
+
+void	start_push_swap(char **av, int ac)
+{
+	int	stack_a[ac - 1];
+	int	stack_b[ac - 1];
+	int	i;
+	int	len;
+
+	i = 0;
+	len = ac - 1;
+	while (i < len)
+	{
+		stack_a[i] = *av[ac - 1];
+		ac--;
+		i++;
+	}
 }
